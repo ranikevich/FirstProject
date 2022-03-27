7ab6e7e4998df8a90974cdbe606d6543a3fd6402
Rimma Anikevich <ranikevich@gmail.com>
2022-03-27 14:09:50 +0700

    Change variables from let to const, left one currency

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let title = "Первый проект";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1500;
-let rollback = 15;
-let fullPrice = 15000;
-let adaptive = true;
+const title = "Первый проект";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 1500;
+const rollback = 15;
+const fullPrice = 15000;
+const adaptive = true;
+const str = screens.toLowerCase();
+const arr = str.split(' ');
 
 alert("Hello");
 console.log("OK");
@@ -12,19 +14,7 @@ console.log (typeof title);
 console.log (typeof fullPrice);
 console.log (typeof adaptive);
 console.log (screens.length);
-
-let screenPriceUSD = Math.round(screenPrice / 102);
-let screenPriceUAH = Math.round(screenPrice / 3.5);
-let screenPriceCNY = Math.round(screenPrice / 15);
-console.log ("Стоимость верстки экранов",screenPrice,"рублей/",screenPriceUSD,"долларов/", screenPriceUAH,"гривен/", screenPriceCNY,"юаней");
-
-let fullPriceUSD = Math.round(fullPrice / 102);
-let fullPriceUAH = Math.round(fullPrice / 3.5);
-let fullPriceCNY = Math.round(fullPrice / 15);
-console.log ("Стоимость разработки сайта",fullPrice,"рублей/",fullPriceUSD,"долларов/", fullPriceUAH,"гривен/", fullPriceCNY,"юаней");
-
-let str = screens.toLowerCase();
-let arr = str.split(' '); 
+console.log ("Стоимость верстки экранов",screenPrice,"рублей");
+console.log ("Стоимость разработки сайта",fullPrice,"рублей"); 
 console.log (arr);
-
-console.log ("Откат посреднику за работу", fullPrice * (rollback / 100));
+console.log ("Откат посреднику за работу", fullPrice * (rollback / 100), "рублей");

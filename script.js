df714f68db1946952a2e87ce88f5dec2974cdb46
Rimma Anikevich <ranikevich@gmail.com>
2022-04-02 00:28:00 +0700

    Change variables and add switch to full price

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const title = "Первый проект";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 1500;
+'use strict';
+
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = +prompt('Сколько будет стоить данная работа?');
 const rollback = 15;
-const fullPrice = 15000;
-const adaptive = true;
+const adaptive = !!prompt('Нужен ли адаптив на сайте?');
 const str = screens.toLowerCase();
 const arr = str.split(' ');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = +prompt('Сколько это будет стоить?');
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.round(fullPrice - fullPrice * (rollback / 100));
+
 
 alert("Hello");
 console.log("OK");
@@ -18,3 +26,20 @@ console.log ("Стоимость верстки экранов",screenPrice,"р�
 console.log ("Стоимость разработки сайта",fullPrice,"рублей"); 
 console.log (arr);
 console.log ("Откат посреднику за работу", fullPrice * (rollback / 100), "рублей");
+
+console.log("Итоговая стоимость за вычетом отката посреднику", servicePercentPrice, "рублей");
+
+switch (true) {
+    case fullPrice >= 30000:
+		console.log('Даем скидку в 10%');
+        break;
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log('Даем скидку в 5%');
+        break;
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log('Скидка не предусмотрена');
+        break;
+    case fullPrice < 0:
+            console.log('Что-то пошло не так');
+            break;
+ }
